6e7ff33a4ef958ad0c4adc6ac79cb18b0c69ec05
lsaoe <fantalsaoe@gmail.com>
2016-05-12 12:14:19 +0800

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -2,16 +2,19 @@ source 'https://gems.ruby-china.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.6'
-gem 'bcrypt',               '3.1.7'
-gem 'bootstrap-sass',       '3.2.0.0'
-gem 'sass-rails',           '5.0.2'
-gem 'uglifier',             '2.5.3'
-gem 'coffee-rails',         '4.1.0'
-gem 'jquery-rails',         '4.0.3'
-gem 'turbolinks',           '2.3.0'
-gem 'jbuilder',             '2.2.3'
-gem 'sdoc',                 '0.4.0', group: :doc
+gem 'rails',                   '4.2.6'
+gem 'bcrypt',                  '3.1.7'
+gem 'faker',                   '1.4.2'
+gem 'will_paginate',           '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
+gem 'bootstrap-sass',          '3.2.0.0'
+gem 'sass-rails',              '5.0.2'
+gem 'uglifier',                '2.5.3'
+gem 'coffee-rails',            '4.1.0'
+gem 'jquery-rails',            '4.0.3'
+gem 'turbolinks',              '2.3.0'
+gem 'jbuilder',                '2.2.3'
+gem 'sdoc',                    '0.4.0', group: :doc
 
 group :development, :test do
   gem 'sqlite3',     '1.3.9'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -6,6 +13,12 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   def create
@@ -19,10 +32,45 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
   private
 
     def user_params
-      params.require(:user).permit(:name,:email, :password,
+      params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # 事前过滤器
+
+    #确保用户已登录
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # 确保是正确用户
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # 确保是管理员
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
